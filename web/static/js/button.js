e944dd16a947c0aac1c26b8519a000569f0c6940
火山火山 <2741547963@qq.com>
2023-04-26 21:24:09 +0800

    更新了button的动画

--- web/static/js/button.js
+++ web/static/js/button.js
@@ -1,14 +1,18 @@
 const html = document.documentElement;
 var btn = document.getElementById("up");
 
+console.log(html);
+
 window.onscroll = () => {
-    let scrollDis = document.documentElement.scrollTop;
-    if (scrollDis > 500) {
-        btn.style.display = "block";
-        console.log("Yes");
-    }
-    else {
-        btn.style.display = "none";
-        console.log("No");
-    }
-}
+  let scrollDis = document.documentElement.scrollTop;
+  if (scrollDis > 500) {
+    btn.style.display = "block";
+
+    btn.onclick = function () {
+      html.scrollIntoView({ behavior: "smooth" });
+      console.log("YES");
+    };
+  } else {
+    btn.style.display = "none";
+  }
+};
